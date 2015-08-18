f4be9e55eaf04246478da99bfc5ec0092c7019ab
Emily Puth <emily.puth@gmail.com>
2015-08-17 20:25:46 -0700

    Create compute-statistics.cpp

---
+++ compute-statistics.cpp
@@ -0,0 +1,19 @@
+//
+// Name: Emily Puth
+// ID: 28239807
+//
+// Reverse String is a program that reverses the
+// contents of a C-string
+
+#ifndef PROBLEM_THREE_HPP_
+#define PROBLEM_THREE_HPP_
+
+#include <iostream>
+#include <cstring>
+
+// Accepts a C-string as input and reverses the contents
+// of the string
+char *reverseCString(char s[]);
+
+
+#endif /* PROBLEM_THREE_HPP_ */
